a6536170e14d6853d732ff4c93e7eb5b07074f14
varunshukla1998 <114413369+varunshukla1998@users.noreply.github.com>
2022-10-14 18:36:25 +0530

    first save
    N/A

---
+++ HelloWorld.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main() {
+	printf("\n \t Name \t\t : Varun Shukla \n \n");
+	printf("\t Contact \t : 8980596921 \n \n");
+	printf("\t Address \t : Drive-in-road, \n \t\t\t   Thaltej, Ahmedabad 380054 \n \n");
+	printf("\t Email Id \t : varunshuklavarunshukla097@gmail.com \n \n");
+	printf("\t DOB \t \t : 28/04/1998 \n \n");
+	printf("\t Qualification   : PG Diploma \n \n");
+	printf("\t Age \t\t : 24 \n \n");
+}
