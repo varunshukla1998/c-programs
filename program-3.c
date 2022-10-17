e0e39f8fe2363e8a3b2da4aadc7a2ea4cd9c8fa2
varunshukla1998 <114413369+varunshukla1998@users.noreply.github.com>
2022-10-17 19:11:49 +0530

    Static Variable Example

---
+++ program-3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+	int num1, num2, ans;
+	
+	num1 = 30;
+	num2 = 20;
+	
+	printf("\n First Number is %d", num1);
+	printf("\n Second Number is %d", num2);
+	
+	ans = num1 - num2;
+	
+	printf("\n Subtraction Of both is %d", ans);
+}
